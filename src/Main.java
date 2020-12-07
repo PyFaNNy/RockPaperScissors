cd6e47c0850eb12b50244697056284812b44e5b6
Artyom <fanny.koob@gmail.com>
2020-12-08 00:16:48 +0300

    interfece

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,65 @@ import java.util.Scanner;
 
 
 public class Main {
-     enum Move {
-        ROCK,
-        PAPER,
-        SCISSORS,
-        LIZARD,
-        SPOCK
-    }
-//    public int compareMoves() {
-//        // Ничья
-//        if (this == otherMove)
-//            return 0;
-//
-//        switch (this) {
-//            case ROCK:
-//
-//            case PAPER:
-//                return (otherMove == ROCK ? 1 : -1);
-//            case SCISSORS:
-//                return (otherMove == PAPER ? 1 : -1);
-//        }
-//    }
 
     public static void main(String[] args) {
-        for (String i:args){
-            System.out.println(i);
-        }
         if (args.length<3){
             System.err.println("Not enough arguments passed");
             System.exit(-1);
         }
         for (String i:args){
             if (!(i.equals("PAPER") || i.equals("ROCK") || i.equals("SCISSORS") || i.equals("LIZARD") || i.equals("SPOCK"))){
-                System.err.println("Invalid commands. Example:PAPER, ROCK, SCISSORS, LIZARD, SPOCK");
+                System.err.println("Invalid commands. Example: PAPER, ROCK, SCISSORS, LIZARD, SPOCK");
                 System.exit(-1);
             }
         }
 
-        
+        for (int i=0;i<args.length;i++){
+            menu();
+        }
+    }
+
+
+
+    public static void menu(){
+        System.out.println("Available moves:");
+        System.out.println("1 - rock");
+        System.out.println("2 - paper");
+        System.out.println("3 - scissors");
+        System.out.println("4 - lizard");
+        System.out.println("5 - Spock");
+        System.out.println("0 - exit");
+        System.out.println("Enter your move:");
+    }
+    public static void result(int num, int len){
+        System.out.print("+");
+        printDashes(68);
+        System.out.println("+");
+
+        System.out.printf("|  ПОБЕДА  |  ПОРАЖЕНИЕ  |  НИЧЬЯ  |  ВСЕГО ИГР  |  ПРОЦЕНТ ПОБЕД  |\n");
+
+        System.out.print("|");
+        printDashes(10);
+        System.out.print("+");
+        printDashes(10);
+        System.out.print("+");
+        printDashes(10);
+        System.out.print("+");
+        printDashes(16);
+        System.out.print("+");
+        printDashes(18);
+        System.out.println("|");
+
+        System.out.printf("|  num  |  len-num  |  len  |  %12d  |  %13.2f%%  |\n",
+                wins, losses, ties, numberOfGames, percentageWon * 100);
+
+        System.out.print("+");
+        printDashes(68);
+        System.out.println("+");
+    }
+    private static void printDashes(int numberOfDashes) {
+        for (int i = 0; i < numberOfDashes; i++) {
+            System.out.print("-");
+        }
     }
 }
